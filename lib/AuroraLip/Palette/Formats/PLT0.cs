0404cc9476df221b97add6bc8e6bf262c8b3a802
Venomalia <Venomalia@mailbox.org>
2022-07-28 23:46:41 +0200

    TEX0 Add Single pallet support

---
+++ lib/AuroraLip/Palette/Formats/PLT0.cs
@@ -0,0 +1,45 @@
+﻿using AuroraLip.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AuroraLip.Texture.J3D.JUtility;
+
+namespace AuroraLip.Palette.Formats
+{
+    public class PLT0: IMagicIdentify
+    {
+        public string Magic => magic;
+
+        private const string magic = "PLT0";
+
+        public GXPaletteFormat PaletteFormat { get; set; }
+
+        public byte[] PaletteData { get; set; }
+
+        public PLT0() {}
+
+        public PLT0(Stream stream) => Read(stream);
+
+        protected void Read(Stream stream)
+        {
+            if (!stream.MatchString(magic))
+                throw new Exception($"Invalid Identifier. Expected \"{Magic}\"");
+            uint TotalSize = stream.ReadUInt32(Endian.Big);
+            uint FormatVersion = stream.ReadUInt32(Endian.Big);
+            uint Offset = stream.ReadUInt32(Endian.Big);
+
+            uint SectionOffsets = stream.ReadUInt32(Endian.Big);
+            uint StringOffset = stream.ReadUInt32(Endian.Big);
+            PaletteFormat = (GXPaletteFormat)stream.ReadUInt32(Endian.Big);
+            short colors = stream.ReadInt16(Endian.Big);
+            ushort pad = stream.ReadUInt16(Endian.Big);
+            uint PathOffset = stream.ReadUInt32(Endian.Big);
+            uint DataOffset = stream.ReadUInt32(Endian.Big);
+            stream.Seek(SectionOffsets,SeekOrigin.Begin);
+            PaletteData = stream.Read(colors*2);
+        }
+    }
+}
--- lib/AuroraLip/Texture/Formats/TEX0.cs
+++ lib/AuroraLip/Texture/Formats/TEX0.cs
@@ -1,4 +1,7 @@
-﻿using AuroraLip.Common;
+﻿using AuroraLip.Archives;
+using AuroraLip.Archives.Formats;
+using AuroraLip.Common;
+using AuroraLip.Palette.Formats;
 using AuroraLip.Texture.J3D;
 using System;
 using System.IO;
@@ -46,24 +49,60 @@ namespace AuroraLip.Texture.Formats
             }
             uint StringOffset = stream.ReadUInt32(Endian.Big);
             //TEX0 Header
-            uint Unknown = stream.ReadUInt32(Endian.Big);
+            uint Unknown = stream.ReadUInt32(Endian.Big); //1
             int ImageWidth = stream.ReadUInt16(Endian.Big);
             int ImageHeight = stream.ReadUInt16(Endian.Big);
             GXImageFormat Format = (GXImageFormat)stream.ReadUInt32(Endian.Big);
             int TotalImageCount = stream.ReadUInt16(Endian.Big);
-            uint Unknown2 = stream.ReadUInt32(Endian.Big);
+            uint Unknown2 = stream.ReadUInt32(Endian.Big); // 65536
             uint Mipmaps = stream.ReadUInt32(Endian.Big);
             uint Unknown3 = stream.ReadUInt32(Endian.Big);
-            stream.Position = SectionOffsets;
+
 
             if (PaletteData == null && JUtility.IsPaletteFormat(Format))
             {
+                if (stream is SubStream substream)
+                {
+                    lock (substream.BaseStream)
+                    {
+
+                        substream.BaseStream.Seek(substream.Offset + StringOffset, SeekOrigin.Begin);
+                        string name = substream.BaseStream.ReadString();
+                        substream.BaseStream.Seek(0, SeekOrigin.Begin);
+                        using (bres parent = new bres(substream.BaseStream))
+                        {
+                            if (parent.ItemExists("root/Palettes(NW4R)"))
+                            {
+                                var Pallets = ((ArchiveDirectory)parent["root/Palettes(NW4R)"]).FindItems(name + "*");
+                                if (Pallets.Count == 1)
+                                {
+                                    var Pallet = new PLT0(((ArchiveFile)parent[Pallets[0]]).FileData);
+                                    PaletteFormat = Pallet.PaletteFormat;
+                                    PaletteData = Pallet.PaletteData;
+                                    PaletteCount = Pallet.PaletteData.Length / 2;
+                                }
+                                else if (Pallets.Count > 1)
+                                {
 #if DEBUG
-                return;
+                                    return;
 #else
-                throw new Exception("TEX0 palette formats are not yet supported");
+                                    throw new Exception($"multiple palettes ({Pallets.Count}) are not supported");
 #endif
+                                }
+                            }
+                        }
+                    }
+                }
+                if (PaletteData == null)
+                {
+#if DEBUG
+                    return;
+#else
+                    throw new Exception("No palette data could be found");
+#endif
+                }
             }
+            stream.Position = SectionOffsets;
             Add(new TexEntry(stream, PaletteData, Format, PaletteFormat, PaletteCount, ImageWidth, ImageHeight, TotalImageCount - 1)
             {
                 LODBias = 0,

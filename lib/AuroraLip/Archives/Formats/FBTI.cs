8dce6df1d790231f10bcf4a91382bf6f08608d99
iwubcode <iwubcode@users.noreply.github.com>
2022-08-19 23:02:55 -0500

    Rune Factory Frontier/Tides support

---
+++ lib/AuroraLip/Archives/Formats/FBTI.cs
@@ -0,0 +1,57 @@
+﻿using AuroraLip.Common;
+using System;
+using System.IO;
+
+namespace AuroraLip.Archives.Formats
+{
+    // Rune Factory (Frontier) archive format
+    public class FBTI : Archive, IMagicIdentify, IFileAccess
+    {
+        public bool CanRead => true;
+
+        public bool CanWrite => false;
+
+        public string Magic => magic;
+
+        private const string magic = "FBTI";
+
+        public FBTI() { }
+
+        public FBTI(string filename) : base(filename) { }
+
+        public FBTI(Stream stream, string filename = null) : base(stream, filename) { }
+
+        public bool IsMatch(Stream stream, in string extension = "")
+            => stream.MatchString(magic);
+
+        protected override void Read(Stream stream)
+        {
+            if (!stream.MatchString(magic))
+                throw new Exception($"Invalid Identifier. Expected \"{Magic}\"");
+            string version = stream.ReadString(4);
+            uint file_count = stream.ReadUInt32(Endian.Big);
+            uint unknown = stream.ReadUInt32(Endian.Big);
+
+            Root = new ArchiveDirectory() { OwnerArchive = this };
+            for (uint i = 0; i < file_count; i++)
+            {
+                uint file_offset = stream.ReadUInt32(Endian.Big);
+                uint size = stream.ReadUInt32(Endian.Big);
+                long saved_position = stream.Position;
+
+                ArchiveFile Sub = new ArchiveFile() { Parent = Root, Name = i.ToString() };
+                stream.Seek(file_offset, SeekOrigin.Begin);
+                Sub.FileData = new SubStream(stream, size);
+                Root.Items.Add(Sub.Name, Sub);
+
+                // Read the file, move on to the next one
+                stream.Seek(saved_position, SeekOrigin.Begin);
+            }
+        }
+
+        protected override void Write(Stream ArchiveFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lib/AuroraLip/Archives/Formats/NLCM.cs
@@ -0,0 +1,75 @@
+﻿using AuroraLip.Common;
+using System;
+using System.IO;
+
+namespace AuroraLip.Archives.Formats
+{
+    // Rune Factory (Frontier) archive format
+    // Cross-referenced with https://github.com/master801/Rune-Factory-Frontier-Tools
+    public class NLCM : Archive, IMagicIdentify, IFileAccess
+    {
+        public bool CanRead => true;
+
+        public bool CanWrite => false;
+
+        public string Magic => magic;
+
+        private const string magic = "NLCM";
+
+        private FileStream reference_stream;
+
+        public NLCM() { }
+
+        public NLCM(string filename) : base(filename) { }
+
+        public NLCM(Stream stream, string filename = null) : base(stream, filename) { }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                if (reference_stream != null)
+                {
+                    reference_stream.Dispose();
+                }
+            }
+        }
+
+        public bool IsMatch(Stream stream, in string extension = "")
+            => stream.MatchString(magic);
+
+        protected override void Read(Stream stream)
+        {
+            if (!stream.MatchString(magic))
+                throw new Exception($"Invalid Identifier. Expected \"{Magic}\"");
+            uint table_offset = stream.ReadUInt32(Endian.Big);
+            uint unknown2 = stream.ReadUInt32(Endian.Big);
+            uint file_count = stream.ReadUInt32(Endian.Big);
+            uint unknown3 = stream.ReadUInt32(Endian.Big);
+            string reference_file = Path.Combine(Directory.GetParent(((System.IO.FileStream)stream).Name).FullName, stream.ReadString());
+            stream.Seek(table_offset, SeekOrigin.Begin);
+            reference_stream = new FileStream(reference_file,
+                       FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            Root = new ArchiveDirectory() { OwnerArchive = this };
+            for (uint i = 0; i < file_count; i++)
+            {
+                uint size = stream.ReadUInt32(Endian.Big);
+                uint padding = stream.ReadUInt32(Endian.Big);
+                uint file_offset = stream.ReadUInt32(Endian.Big);
+                uint padding2 = stream.ReadUInt32(Endian.Big);
+
+                ArchiveFile Sub = new ArchiveFile() { Parent = Root, Name = i.ToString() };
+                reference_stream.Seek(file_offset, SeekOrigin.Begin);
+                Sub.FileData = new SubStream(reference_stream, size);
+                Root.Items.Add(Sub.Name, Sub);
+            }
+        }
+
+        protected override void Write(Stream ArchiveFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

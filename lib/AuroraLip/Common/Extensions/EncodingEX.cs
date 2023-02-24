f66844c1c28d62fec64b2bafd76ab20e76534191
Venomalia <Venomalia@mailbox.org>
2023-02-24 17:09:17 +0100

    Update ToValidString

--- lib/AuroraLip/Common/Extensions/EncodingEX.cs
+++ lib/AuroraLip/Common/Extensions/EncodingEX.cs
@@ -14,8 +14,9 @@ namespace AuroraLip.Common
         internal static readonly Predicate<byte> AllValidBytes_ASKI = b => b >= 32 && b < 127;
 
         internal static readonly Predicate<byte> AllValidBytes = b => b >= 32 && b != 127;
+
         /// <summary>
-        /// 
+        /// Decodes all the Valid bytes in the specified byte array into a string.
         /// </summary>
         /// <param name="bytes"></param>
         /// <param name="validbytes">Determines if the byte is valid</param>
@@ -23,8 +24,9 @@ namespace AuroraLip.Common
         [DebuggerStepThrough]
         public static string ToValidString(this byte[] bytes, Predicate<byte> validbytes = null)
             => bytes.ToValidString(DefaultEncoding, validbytes);
+
         /// <summary>
-        /// 
+        /// Decodes all the Valid bytes in the specified byte array into a string.
         /// </summary>
         /// <param name="bytes"></param>
         /// <param name="Encoding">Encoding to use when getting the string</param>
@@ -37,6 +39,37 @@ namespace AuroraLip.Common
 
             List<byte> magicbytes = new List<byte>();
             foreach (byte b in bytes) if (validbytes.Invoke(b)) magicbytes.Add(b);
+            return Encoding.GetString(magicbytes.ToArray());
+        }
+
+        /// <summary>
+        /// Decodes all the Valid bytes in the specified byte array into a string.
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="start"></param>
+        /// <param name="length"></param>
+        /// <param name="validbytes"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static string ToValidString(this byte[] bytes, int start, int length, Predicate<byte> validbytes = null)
+            => bytes.ToValidString(DefaultEncoding, start, length, validbytes);
+        /// <summary>
+        /// Decodes all the Valid bytes in the specified byte array into a string.
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="Encoding"></param>
+        /// <param name="start"></param>
+        /// <param name="length"></param>
+        /// <param name="validbytes"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static string ToValidString(this byte[] bytes, Encoding Encoding, int start, int length, Predicate<byte> validbytes = null)
+        {
+            if (validbytes == null) validbytes = GetValidbytesPredicate(Encoding);
+
+            List<byte> magicbytes = new List<byte>();
+            for (int i = start; i < start + length; i++)
+                if (validbytes.Invoke(bytes[i])) magicbytes.Add(bytes[i]);
             return Encoding.GetString(magicbytes.ToArray());
         }
         /// <summary>
@@ -64,15 +97,6 @@ namespace AuroraLip.Common
         [DebuggerStepThrough]
         public static char ToChar(this byte Byte)
             => DefaultEncoding.GetChars(new byte[] { Byte })[0];
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="Byte"></param>
-        /// <param name="Encoding">Encoding to use when getting the char</param>
-        /// <returns></returns>
-        [DebuggerStepThrough]
-        public static char ToChar(this byte Byte, Encoding Encoding)
-            => Encoding.GetChars(new byte[] { Byte })[0];
         /// <summary>
         /// 
         /// </summary>

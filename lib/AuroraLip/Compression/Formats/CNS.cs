8a3a65cce1c06cf38ecb4cb9771354d23fbf9477
Venomalia <Venomalia@mailbox.org>
2023-06-18 18:23:14 +0200

    add CNS Compression method

--- lib/AuroraLip/Compression/Formats/CNS.cs
+++ lib/AuroraLip/Compression/Formats/CNS.cs
@@ -10,13 +10,11 @@ namespace AuroraLip.Compression.Formats
     {
         public bool CanRead => true;
 
-        public bool CanWrite => false;
+        public bool CanWrite => true;
 
         public string Magic => magic;
 
         public const string magic = "@CNS";
-
-        public void Compress(in byte[] source, Stream destination) => throw new NotImplementedException();
 
         public byte[] Decompress(Stream source)
         {
@@ -26,6 +24,23 @@ namespace AuroraLip.Compression.Formats
             source.Position += 4; // 0
 
             return Decompress_ALG(source, (int)decompressedSize);
+        }
+
+        public void Compress(in byte[] source, Stream destination)
+        {
+            destination.Write(magic);
+            if (source[0] == 0x0 && source[1] == 0x20 && source[2] == 0xAF && source[3] == 0x30)
+            {
+                destination.WriteString("TPL");
+            }
+            else
+            {
+                destination.WriteString("PAK");
+            }
+            destination.Write(source.Length, Endian.Little);
+            destination.Write(0);
+
+            Compress_ALG(source, destination);
         }
 
         public bool IsMatch(Stream stream, in string extension = "")
@@ -65,6 +80,70 @@ namespace AuroraLip.Compression.Formats
                 }
             }
             return destination;
+        }
+
+
+        public static void Compress_ALG(byte[] sourceArray, Stream destination)
+        {
+            int sourceLength = sourceArray.Length,
+                sourcePointer = 0x0;
+
+            byte data1, data2;
+
+            // Initalize the LZ dictionary
+            LzWindowDictionary dictionary = new();
+            dictionary.SetWindowSize(255);
+            dictionary.SetMaxMatchAmount(130);
+
+            using (MemoryStream buffer = new(255))
+            {
+                // Start compression
+                while (sourcePointer < sourceLength)
+                {
+                    // Search for a match
+                    int[] match = dictionary.Search(sourceArray, (uint)sourcePointer, (uint)sourceLength);
+
+
+                    if ((buffer.Length != 0 && match[1] > 0) || buffer.Length == 127)
+                    {
+                        destination.WriteByte((byte)buffer.Length);
+                        buffer.WriteTo(destination);
+                        buffer.SetLength(0);
+                    }
+
+                    if (match[1] > 0) // There is a match
+                    {
+
+                        data1 = (byte)(0x80 | (match[1] - 3));
+                        data2 = (byte)(match[0] - 1);
+                        destination.WriteByte(data1);
+                        destination.WriteByte(data2);
+
+                        dictionary.AddEntryRange(sourceArray, sourcePointer, match[1]);
+
+                        sourcePointer += match[1];
+                    }
+                    else // There is not a match
+                    {
+                        buffer.WriteByte(sourceArray[sourcePointer]);
+
+                        dictionary.AddEntry(sourceArray, sourcePointer);
+
+                        sourcePointer++;
+                    }
+
+                    // Check to see if we reached the end of the file
+                    if (sourcePointer >= sourceLength)
+                        break;
+                }
+
+                if (buffer.Length != 0)
+                {
+                    destination.WriteByte((byte)buffer.Length);
+                    buffer.WriteTo(destination);
+                    buffer.SetLength(0);
+                }
+            }
         }
     }
 }

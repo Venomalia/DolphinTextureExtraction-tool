6832dfb0889beaea6e9503c70ce62ba5494ee24f
Venomalia <Venomalia@mailbox.org>
2023-06-20 19:54:06 +0200

    add ALTX Aqualead Texture support
    from Pandora's Tower

---
+++ lib/AuroraLip/Texture/Formats/ALTX.cs
@@ -0,0 +1,25 @@
+﻿using AuroraLib.Common;
+
+namespace AuroraLib.Texture.Formats
+{
+    internal class ALTX : ALIG
+    {
+        public override string Magic => magic;
+
+        private const string magic = "ALTX";
+
+
+        protected override void Read(Stream stream)
+        {
+            if (!stream.MatchString(magic))
+                throw new InvalidIdentifierException(Magic);
+
+            uint unk = stream.ReadUInt32();
+            uint Offset = stream.ReadUInt32(Endian.Big);
+
+            stream.Seek(Offset, SeekOrigin.Begin);
+
+            base.Read(stream);
+        }
+    }
+}

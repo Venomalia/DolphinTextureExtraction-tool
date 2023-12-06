049d7d0032691d15a0c16a3bacb31a3016865085
Venomalia <87765258+Venomalia@users.noreply.github.com>
2023-12-06 15:42:24 +0100

    Rune Factory Texture Palleten data are processed properly

--- lib/AuroraLip/Texture/Formats/HXTB.cs
+++ lib/AuroraLip/Texture/Formats/HXTB.cs
@@ -32,51 +32,51 @@ namespace AuroraLib.Texture.Formats
         {
             stream.MatchThrow(_identifier);
 
-            string version = stream.ReadString(4);
+            int version = int.Parse(stream.ReadString(4));
             uint name_table_offset = stream.ReadUInt32(Endian.Big);
             uint file_count = stream.ReadUInt32(Endian.Big);
+            //stream.Position += 8;
+            //uint data_Size = stream.ReadUInt32(Endian.Big);
+            //uint file_Size = stream.ReadUInt32(Endian.Big);
+
             // File size and other data after this
             for (uint i = 0; i < file_count; i++)
             {
                 stream.Seek(name_table_offset + i * 0x20, SeekOrigin.Begin);
                 string name = stream.ReadString(16);
-                uint unknown = stream.ReadUInt32(Endian.Big);
+                uint hash = stream.ReadUInt32(Endian.Big);
                 uint header_offset = stream.ReadUInt32(Endian.Big);
                 uint palette_header_offset = stream.ReadUInt32(Endian.Big);
                 stream.Seek(header_offset, SeekOrigin.Begin);
                 uint data_offset = stream.ReadUInt32(Endian.Big);
-                GXImageFormat format = (GXImageFormat)stream.ReadByte();
-                byte image_count = (byte)stream.ReadByte(); // max_LOD?
-                ushort unknown3 = stream.ReadUInt16(Endian.Big);
+                GXImageFormat format = stream.Read<GXImageFormat>();
+                byte max_LOD = stream.ReadUInt8();
+                ushort padding = stream.ReadUInt16(Endian.Big); //always 0
                 ushort width = stream.ReadUInt16(Endian.Big);
                 ushort height = stream.ReadUInt16(Endian.Big);
                 uint size = stream.ReadUInt32(Endian.Big);
 
-                //int mipmaps = image_count - 1;
+                int mipmaps = max_LOD == 0 ? 0 : max_LOD - 1;
 
-                //some images have a too large or too small image_count!
-                //We keep calculating the mipmaps until we know why this is so.
-                int mipmaps = format.GetMipmapsFromSize((int)size, width, height);
-
-                byte[] palette_data = null;
+                Span<byte> palette_data = Span<byte>.Empty;
                 GXPaletteFormat palette_format = GXPaletteFormat.IA8;
                 uint palette_count = 0;
-                if (palette_header_offset > 0)
+                if (format.IsPaletteFormat())
                 {
                     stream.Seek(palette_header_offset, SeekOrigin.Begin);
                     uint palette_data_offset = stream.ReadUInt32(Endian.Big);
-                    palette_format = (GXPaletteFormat)stream.ReadByte();
-                    ushort palette_unknown = stream.ReadUInt16(Endian.Big);
+                    palette_format = stream.Read<GXPaletteFormat>();
+                    byte palette_unknown = stream.ReadUInt8();
                     ushort palette_unknown2 = stream.ReadUInt16(Endian.Big);
-                    uint palette_unknown3 = stream.ReadUInt32(Endian.Big);
+                    palette_count = stream.ReadUInt16(Endian.Big);
+                    uint palette_unknown3 = stream.ReadUInt16(Endian.Big); //always 1
                     uint palette_size = stream.ReadUInt32(Endian.Big);
-                    palette_count = palette_size / 2;
                     stream.Seek(palette_data_offset, SeekOrigin.Begin);
                     palette_data = stream.Read(palette_size);
                 }
 
                 stream.Seek(data_offset, SeekOrigin.Begin);
-                TexEntry current = new TexEntry(stream, palette_data, format, palette_format, (int)palette_count, width, height, mipmaps)
+                TexEntry current = new (stream, palette_data, format, palette_format, (int)palette_count, width, height, mipmaps)
                 {
                     LODBias = 0,
                     MagnificationFilter = GXFilterMode.Nearest,
@@ -85,7 +85,7 @@ namespace AuroraLib.Texture.Formats
                     WrapT = GXWrapMode.CLAMP,
                     EnableEdgeLOD = false,
                     MinLOD = 0,
-                    MaxLOD = image_count
+                    MaxLOD = max_LOD
                 };
                 Add(current);
             }

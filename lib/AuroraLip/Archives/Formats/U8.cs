fa6992a1bedc398083f5a158245bbd057b6996c1
Venomalia <87765258+Venomalia@users.noreply.github.com>
2023-12-07 21:42:45 +0100

    rewrite U8 Archive processing
    - All names are read properly
    - Improves performance

--- lib/AuroraLip/Archives/Formats/U8.cs
+++ lib/AuroraLip/Archives/Formats/U8.cs
@@ -1,17 +1,13 @@
 ﻿using AuroraLib.Common;
+using AuroraLib.Core.Buffers;
 using AuroraLib.Core.Interfaces;
-using System.Text;
 
 namespace AuroraLib.Archives.Formats
 {
-    /*
-    * Super Hackio Incorporated
-    * "Copyright © Super Hackio Incorporated 2020-2021"
-    * https://github.com/SuperHackio/Hack.io
-    */
 
+    // Based on https://wiki.tockdom.com/wiki/U8_(File_Format) an https://github.com/SuperHackio/Hack.io
     /// <summary>
-    /// Based on https://wiki.tockdom.com/wiki/U8_(File_Format)
+    /// Nintendo U8 Archive
     /// </summary>
     public class U8 : Archive, IHasIdentifier, IFileAccess
     {
@@ -40,280 +36,149 @@ namespace AuroraLib.Archives.Formats
         protected override void Read(Stream stream)
         {
             stream.MatchThrow(_identifier);
+            uint nodeTableOffset = stream.ReadUInt32(Endian.Big); //usually 0x20
+            uint nodeSectionSize = stream.ReadUInt32(Endian.Big);
+            uint dataSectionOffset = stream.ReadUInt32(Endian.Big);
+            stream.Position += 0x10; //Skip 16 reserved bytes
 
-            uint OffsetToNodeSection = stream.ReadUInt32(Endian.Big); //usually 0x20
-            uint NodeSectionSize = stream.ReadUInt32(Endian.Big);
-            uint FileDataOffset = stream.ReadUInt32(Endian.Big);
-            stream.Position += 0x10; //Skip 16 reserved bytes. All are 0xCC
+            //Read Nods
+            stream.Seek(nodeTableOffset, SeekOrigin.Begin);
+            U8Node rootNode = stream.Read<U8Node>(Endian.Big);
+            using SpanBuffer<U8Node> nodes = new(rootNode.Size - 1);
+            stream.Read(nodes.Span, Endian.Big);
 
-            //Node time
-            //Each node is 0x0C bytes each
-            //The first node is node 0
-
-            //Node format:
-            //0x00 = byte = IsDirectory
-            //0x01 = Int24..... oh no...
-            //0x04 = File: Offset to data start | Directory: Index of Parent Directory
-            //0x08 = File: Size of the File | Directory: Index of the directory's first node?
-
-            //Root has total number of nodes
-            stream.Position = OffsetToNodeSection;
-            U8Node RootNode = new U8Node(stream);
-
-            //Root has total number of nodes
-            int TotalNodeCount = RootNode.Size;
-            long StringTableLocation = OffsetToNodeSection + (TotalNodeCount * 0x0C);
-
-            //Read all our entries
-            List<U8Node> entries = new List<U8Node>
+            long StringTableOffset = nodeTableOffset + (rootNode.Size * 0x0C);
+            Root = new ArchiveDirectory() { Name = "root", OwnerArchive = this };
+            List<ArchiveDirectory> directorys = new() { Root };
+            Stack<Tuple<ArchiveDirectory, int>> stack = new();
+            stack.Push(new(Root, nodes.Length));
+            for (int i = 0; i < nodes.Length; i++)
             {
-                RootNode
-            };
-            List<ArchiveObject> FlatItems = new List<ArchiveObject>();
-            //entries.Add(RootNode);
-            for (int i = 0; i < TotalNodeCount; i++)
-            {
-                var node = new U8Node(stream);
-                entries.Add(node);
-                long PausePosition = stream.Position;
-                if (entries[i].IsDirectory)
+                U8Node node = nodes[i];
+                stream.Seek(StringTableOffset + node.NameOffset, SeekOrigin.Begin);
+                string name = stream.ReadString();
+
+                //if there is already an ellement with the names.
+                if (stack.Peek().Item1.Items.ContainsKey(name))
                 {
-                    ArchiveDirectory dir = new ArchiveDirectory() { OwnerArchive = this };
-                    stream.Position = StringTableLocation + entries[i].NameOffset;
-                    dir.Name = stream.ReadString(); // x => x != 0
-                    FlatItems.Add(dir);
+                    name = $"{Path.GetFileNameWithoutExtension(name.AsSpan())}_{i}{Path.GetExtension(name.AsSpan())}";
+                }
+
+                if (node.IsDirectory)
+                {
+                    ArchiveDirectory dir = new() { OwnerArchive = this, Parent = directorys[node.Offset], Name = name };
+                    directorys[node.Offset].Items.Add(name, dir);
+                    directorys.Add(dir);
+                    stack.Push(new(dir, node.Size));
                 }
                 else
                 {
-                    ArchiveFile file = new ArchiveFile() { OwnerArchive = this };
-                    stream.Position = StringTableLocation + entries[i].NameOffset;
-                    file.Name = stream.ReadString();
-                    stream.Position = entries[i].DataOffset;
-                    file.FileData = new MemoryStream(stream.Read(entries[i].Size));
-                    FlatItems.Add(file);
+                    stack.Peek().Item1.AddArchiveFile(stream, node.Size, node.Offset, name);
                 }
-                stream.Position = PausePosition;
-            }
-            entries.RemoveAt(entries.Count - 1);
-            Stack<ArchiveDirectory> DirectoryStack = new Stack<ArchiveDirectory>();
-            DirectoryStack.Push((ArchiveDirectory)FlatItems[0]);
-            for (int i = 1; i < entries.Count; i++)
-            {
-                if (entries[i].IsDirectory)
-                {
-                    int parent = entries[i].DataOffset;
-                    int EntryCount = entries[i].Size;
 
-                    if (FlatItems[parent] is ArchiveDirectory dir)
-                    {
-                        ArchiveDirectory curdir = (ArchiveDirectory)FlatItems[i];
-                        if (dir.Items.ContainsKey(curdir.Name))
-                        {
-                            for (int n = 1; true; n++)
-                            {
-                                if (!dir.Items.ContainsKey($"{curdir.Name}_{n}"))
-                                {
-                                    curdir.Name = $"{curdir.Name}_{n}";
-                                    break;
-                                }
-                            }
-                        }
-                        dir.Items.Add(curdir.Name, curdir);
-                        curdir.Parent = dir;
-                    }
-                    DirectoryStack.Push((ArchiveDirectory)FlatItems[i]);
-                }
-                else
+                if (i == stack.Peek().Item2 - 1)
                 {
-                    //if there is already an ellement with the names.
-                    if (DirectoryStack.Peek().Items.ContainsKey((FlatItems[i]).Name))
-                    {
-                        string name = ((ArchiveFile)FlatItems[i]).Name;
-                        ((ArchiveFile)FlatItems[i]).Name = $"{Path.GetFileNameWithoutExtension(name.AsSpan()).ToString()}_{i}{Path.GetExtension(name.AsSpan()).ToString()}";
-                    }
-
-                    FlatItems[i].Parent = DirectoryStack.Peek();
-                    DirectoryStack.Peek().Items.Add((FlatItems[i]).Name, FlatItems[i]);
-                }
-                if (i == entries[FlatItems.IndexOf(DirectoryStack.Peek())].Size - 1)
-                {
-                    DirectoryStack.Pop();
+                    stack.Pop();
                 }
             }
-            Root = (ArchiveDirectory)FlatItems[0];
         }
 
         protected override void Write(Stream stream)
         {
-            List<dynamic> FlatItems = new List<dynamic>();
+            // Create Note Tabel
+            U8Node rootNode = new(true, 0, 0, TotalFileCount + 1);
+            using SpanBuffer<U8Node> nodeTabel = new(rootNode.Size);
+            nodeTabel[0] = rootNode;
 
-            AddItems(Root);
-            //The archive has been flattened hooray
-            Dictionary<string, uint> StringOffsets = new Dictionary<string, uint>();
-            List<byte> StringBytes = GetStringTableBytes(FlatItems, ref StringOffsets);
+            // Build String Tabel
+            using MemoryPoolStream stringTabel = new();
+            Dictionary<string, UInt24> stringOffsets = new(nodeTabel.Length);
+            stringTabel.WriteByte(0); // Root byte
+            BuildStringTabel(Root);
 
-            uint DataOffset = (uint)(0x20 + (FlatItems.Count * 0x0C) + StringBytes.Count);
-            DataOffset += 0x20 - (DataOffset % 0x20);
-            //while (DataOffset % 16 != 0)
-            //    DataOffset++;
-            Dictionary<ArchiveFile, uint> DataOffsets = new Dictionary<ArchiveFile, uint>();
-            List<byte> DataBytes = GetDataBytes(FlatItems, DataOffset, ref DataOffsets);
-
-            List<U8Node> Nodes = new List<U8Node>();
-            Stack<ArchiveDirectory> DirectoryStack = new Stack<ArchiveDirectory>();
-            for (int i = 0; i < FlatItems.Count; i++)
-            {
-                U8Node newnode = new U8Node() { NameOffset = new Int24((int)StringOffsets[FlatItems[i].Name]) };
-                if (FlatItems[i] is ArchiveDirectory dir)
-                {
-                    if (DirectoryStack.Count > 1)
-                        while (!object.ReferenceEquals(DirectoryStack.Peek(), dir.Parent))
-                        {
-                            Nodes[FlatItems.IndexOf(DirectoryStack.Peek())].Size = i;
-                            DirectoryStack.Pop();
-                        }
-                    newnode.IsDirectory = true;
-                    if (i != 0) //Node is not the Root
-                    {
-                        newnode.DataOffset = FlatItems.IndexOf(dir.Parent);
-                    }
-                    else
-                    {
-                        newnode.Size = FlatItems.Count;
-                    }
-                    DirectoryStack.Push(dir);
-                }
-                else
-                {
-                    newnode.DataOffset = (int)DataOffsets[(ArchiveFile)FlatItems[i]];
-                    newnode.Size = (int)((ArchiveFile)FlatItems[i]).FileData.Length;
-                }
-                Nodes.Add(newnode);
-                if (DirectoryStack.Peek().Items.Count == 0 || object.ReferenceEquals(FlatItems[i], DirectoryStack.Peek().Items.Last().Value))
-                {
-                    int index = FlatItems.IndexOf(DirectoryStack.Peek());
-                    Nodes[index].Size = i + 1;
-                    DirectoryStack.Pop();
-                }
-            }
-
-            while (DirectoryStack.Count > 0)
-            {
-                Nodes[FlatItems.IndexOf(DirectoryStack.Peek())].Size = Nodes.Count;
-                DirectoryStack.Pop();
-            }
-
-            //Write the Header
+            //Write Header
+            uint nodeSectionSize = (uint)(nodeTabel.Length * 0x0C + stringTabel.Length);
+            uint dataSectionOffset = nodeSectionSize + 0x20;
             stream.Write(_identifier);
+            stream.Write(0x20, Endian.Big); // Node Table Offset
+            stream.Write(nodeSectionSize, Endian.Big); // Node Section Size
+            stream.Write(dataSectionOffset, Endian.Big); // File Data Offset
+            stream.Write(0, 4); // reserved bytes
 
-            stream.Write(0x20, Endian.Big);
-            stream.Write(Nodes.Count * 0x0C + StringBytes.Count, Endian.Big);
-            stream.Write(DataOffset, Endian.Big);
-            stream.Write(DataOffset, Endian.Big);
-            stream.Write(new byte[16] { 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC });
+            //Write Node Section
+            long nodeSectionPosition = stream.Position;
+            stream.Write<U8Node>(nodeTabel); // Node Tabel Placeholder
+            stringTabel.WriteTo(stream); // String Tabel
 
-            //Write the Nodes
-            for (int i = 0; i < Nodes.Count; i++)
-                Nodes[i].Write(stream);
+            //Write Data Section
+            long DataSectionPosition = stream.Position;
+            List<ArchiveDirectory> directorys = new() { Root };
+            WriteDataSection(Root, 1);
 
-            //Write the strings
-            stream.Write(StringBytes.ToArray(), 0, StringBytes.Count);
-            stream.WriteAlign(0x20, 0);
+            //Write Real Nodes
+            stream.At(nodeSectionPosition, s => s.Write<U8Node>(nodeTabel, Endian.Big));
 
-            //Write the File Data
-            stream.Write(DataBytes.ToArray(), 0, DataBytes.Count);
-
-            void AddItems(ArchiveDirectory dir)
+            void BuildStringTabel(ArchiveDirectory parent)
             {
-                FlatItems.Add(dir);
-                List<ArchiveDirectory> subdirs = new List<ArchiveDirectory>();
-                foreach (var item in dir.Items)
+                foreach (ArchiveObject item in parent.Items.Values)
                 {
-                    if (item.Value is ArchiveDirectory d)
-                        subdirs.Add(d);
-                    else
-                        FlatItems.Add(item.Value);
+                    if (!stringOffsets.ContainsKey(item.Name))
+                    {
+                        stringOffsets.Add(item.Name, (UInt24)stringTabel.Position);
+                        stringTabel.WriteString(item.Name, (byte)0);
+                    }
+                    if (item is ArchiveDirectory directory)
+                    {
+                        BuildStringTabel(directory);
+                    }
                 }
-                for (int i = 0; i < subdirs.Count; i++)
-                    AddItems(subdirs[i]);
+            }
+
+            void WriteDataSection(ArchiveDirectory parent, int index)
+            {
+                // Process the files first then the directories.
+                foreach (ArchiveObject item in parent.Items.Values)
+                {
+                    if (item is ArchiveFile file)
+                    {
+                        UInt24 nameOffset = stringOffsets.GetValueOrDefault(file.Name);
+                        int dataOffset = (int)(stream.Position - DataSectionPosition + dataSectionOffset);
+                        nodeTabel[index++] = new(false, nameOffset, dataOffset, (int)file.Size);
+                        file.FileData.Position = 0;
+                        file.FileData.CopyTo(stream);
+                    }
+                }
+                foreach (ArchiveObject item in parent.Items.Values)
+                {
+                    if (item is ArchiveDirectory directory)
+                    {
+                        UInt24 nameOffset = stringOffsets.GetValueOrDefault(directory.Name);
+                        int parentIndex = directorys.IndexOf(directory.Parent);
+                        int itemEndOffset = directory.GetCountAndChildren() + index + 1;
+                        nodeTabel[index++] = new(true, nameOffset, parentIndex, itemEndOffset);
+                        directorys.Add(directory);
+                        WriteDataSection(directory, index);
+                    }
+                }
             }
         }
 
-        #region Internals
-
-        /// <summary>
-        /// Only used when Reading / Writing
-        /// </summary>
-        internal class U8Node
+        internal readonly struct U8Node
         {
-            public bool IsDirectory;
-            public Int24 NameOffset;
-            public int DataOffset;
-            public int Size;
+            private readonly byte isDirectoryByte;
+            public readonly UInt24 NameOffset;
+            public readonly int Offset;
+            public readonly int Size;
 
-            public U8Node()
-            { }
+            public bool IsDirectory => isDirectoryByte == 1;
 
-            public U8Node(Stream stream)
+            public U8Node(bool isDirectory, UInt24 nameOffset, int dataOffset, int size)
             {
-                IsDirectory = stream.ReadByte() == 1;
-                NameOffset = stream.ReadInt24(Endian.Big);
-                DataOffset = stream.ReadInt32(Endian.Big);
-                Size = stream.ReadInt32(Endian.Big);
+                isDirectoryByte = isDirectory ? (byte)1 : (byte)0;
+                NameOffset = nameOffset;
+                Offset = dataOffset;
+                Size = size;
             }
-
-            public void Write(Stream stream)
-            {
-                stream.WriteByte((byte)(IsDirectory ? 0x01 : 0x00));
-                stream.Write(NameOffset, Endian.Big);
-                stream.Write(DataOffset, Endian.Big);
-                stream.Write(Size, Endian.Big);
-            }
-
-            public override string ToString() => $"{(IsDirectory ? "Directory" : "File")}: {NameOffset.ToString()} | {DataOffset.ToString()} | {Size.ToString()}";
         }
-
-        private List<byte> GetDataBytes(List<dynamic> FlatFileList, uint DataStart, ref Dictionary<ArchiveFile, uint> Offsets)
-        {
-            List<byte> FileBytes = new List<byte>();
-            for (int i = 0; i < FlatFileList.Count; i++)
-            {
-                if (!(FlatFileList[i] is ArchiveFile file))
-                    continue;
-
-                if (Offsets.Any(OFF => OFF.Key.FileData.ToArray().SequenceEqual(file.FileData.ToArray())))
-                {
-                    Offsets.Add(file, Offsets[Offsets.Keys.First(FILE => FILE.FileData.ToArray().SequenceEqual(file.FileData.ToArray()))]);
-                }
-                else
-                {
-                    List<byte> CurrentMRAMFile = file.FileData.ToArray().ToList();
-                    while (CurrentMRAMFile.Count % 32 != 0)
-                        CurrentMRAMFile.Add(0x00);
-                    Offsets.Add(file, (uint)FileBytes.Count + DataStart);
-                    FileBytes.AddRange(CurrentMRAMFile);
-                }
-            }
-            return FileBytes;
-        }
-
-        private List<byte> GetStringTableBytes(List<dynamic> FlatFileList, ref Dictionary<string, uint> Offsets)
-        {
-            List<byte> strings = new List<byte>();
-            Encoding enc = Encoding.GetEncoding(932);
-
-            for (int i = 0; i < FlatFileList.Count; i++)
-            {
-                if (!Offsets.ContainsKey(FlatFileList[i].Name))
-                {
-                    Offsets.Add(FlatFileList[i].Name, (uint)strings.Count);
-                    strings.AddRange(enc.GetBytes(FlatFileList[i].Name));
-                    strings.Add(0x00);
-                }
-            }
-            return strings;
-        }
-
-        #endregion Internals
     }
 }
